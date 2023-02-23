199d197f5f4b2e1b34ce6f4b2e194dd159740ece
Nick <nicolas.cinera@gmail.com>
2023-02-22 21:50:42 -0500

    Unit Testing Post

--- src/test/java/com/extrieve/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/extrieve/productservice/ProductServiceApplicationTests.java
@@ -1,27 +1,51 @@
 package com.extrieve.productservice;
 
+import com.extrieve.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+	private final MockMvc mockMvc;
+
+	ProductServiceApplicationTests(MockMvc mockMvc) {
+		this.mockMvc = mockMvc;
+	}
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 	@Test
-	void ensureProductCreation() {
+	void ensureProductCreation() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType("application/json")
+				.content(getProductRequest().toString())
+				).andExpect(status().isCreated());
+	}
 
+	private ProductRequest getProductRequest(){
+		return ProductRequest.builder()
+				.name("Test Product")
+				.description("Test Description")
+				.price("100")
+				.build();
 	}
 
 }
